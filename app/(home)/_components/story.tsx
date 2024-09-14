1e20410130fbfd266031baa751769ebac8c073e4
DavideIeria99 <davideieria99@gmail.com>
2024-09-14 19:26:16 +0200

    story & iAm

---
+++ app/(home)/_components/story.tsx
@@ -0,0 +1,13 @@
+export const Story = () => {
+	return (
+		<section className="ps-5 my-10">
+			<h2 className="uppercase font-bold text-2xl mb-5">storia</h2>
+			<p className="text-justify mx-auto w-1/2">
+				Sviluppatore web junior nella creazione di siti web e applicazioni web
+				personalizzate. Esperienzanella creazione di siti web responsive e
+				ottimizzati per i motori di ricerca. Buona capacità di lavorare in team
+				ma anche singolarmente.
+			</p>
+		</section>
+	);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { Hero } from "./(home)/_components/hero";
+import { Story } from "./(home)/_components/story";
+import { WhoIam } from "./(home)/_components/who-i-am";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
-			<main className="w-full min-h-screen"></main>
+			<main className="w-full min-h-screen">
+				<WhoIam />
+				<Story />
+			</main>
 		</>
 	);
 }
