0bc5dd9185c1fcef4d757ef29b12503a2ef68639
DavideIeria99 <davideieria99@gmail.com>
2025-11-14 19:02:39 +0100

    fix:privacy-mobile

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="mt-10 flex h-20 flex-col-reverse bg-linear-to-t from-slate-200 text-black md:flex-row md:justify-around">
+        <footer className="mt-10  flex h-20 flex-col-reverse bg-linear-to-t from-slate-200 text-black md:flex-row md:justify-around">
             {/* <!--Copyright section--> */}
             <span className="mt-auto mb-4 items-center text-center text-slate-700">
                 © {new Date().getFullYear()} Copyright:Davide Ieria,
